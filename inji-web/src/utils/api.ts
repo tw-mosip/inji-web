355fe39716bfccb67858a0a91d13fcdce7595da0
Vijay <94220135+vijay151096@users.noreply.github.com>
2024-04-25 18:40:26 +0530

    [INJIWEB-247]:  update the configuration
    Signed-off-by: Vijay <94220135+vijay151096@users.noreply.github.com>

--- inji-web/src/utils/api.ts
+++ inji-web/src/utils/api.ts
@@ -8,14 +8,15 @@ export enum MethodType {
 
 export class api {
 
-    static mimotoHost = window.location.origin || "/v1/mimoto";
+    //static mimotoHost = "/v1/mimoto";
+    static mimotoHost = window.location.origin + "/v1/mimoto";
 
-    static authorizationRedirectionUrl = api.mimotoHost + "/redirect";
+    static authorizationRedirectionUrl = window.location.origin + "/redirect";
     static fetchIssuers = () => "/issuers"
-    static searchIssuers = (searchText: string) => `/issuers?search=${searchText}`
-    static fetchSpecificIssuer = (issuerId: string) => `/issuers/${issuerId}`
-    static fetchCredentialTypes = (issuerId: string) => `/issuers/${issuerId}/credentialTypes`
-    static searchCredentialType = (issuerId: string, searchText: string) => `/issuers/${issuerId}/credentialTypes?search=${searchText}`
+    static searchIssuers = (searchText: string) => `${api.mimotoHost}/issuers?search=${searchText}`
+    static fetchSpecificIssuer = (issuerId: string) => `${api.mimotoHost}/issuers/${issuerId}`
+    static fetchCredentialTypes = (issuerId: string) => `${api.mimotoHost}/issuers/${issuerId}/credentialTypes`
+    static searchCredentialType = (issuerId: string, searchText: string) => `${api.mimotoHost}/issuers/${issuerId}/credentialTypes?search=${searchText}`
     static authorization = (currentIssuer: IssuerObject, state: string, code_challenge: CodeChallengeObject) => {
         return `${currentIssuer.authorization_endpoint}` +
             `?response_type=code&` +

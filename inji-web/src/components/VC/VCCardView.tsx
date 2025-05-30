5a147a2b5e7fd9a957d5ec4166f3a80037426992
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2025-05-30 17:24:33 +0530

    [INJIWEB-1522] modify VC card view to show card detials
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- inji-web/src/components/VC/VCCardView.tsx
+++ inji-web/src/components/VC/VCCardView.tsx
@@ -1,44 +1,30 @@
 import {WalletCredential} from "../../types/data";
 
-export function VCCardView(props: { onClick: (credential: WalletCredential) => void, credential: WalletCredential }) {
-    return <div
-
-        className="bg-iw-tileBackground flex flex-col shadow hover:shadow-lg hover:scale-105 hover:shadow-iw-selectedShadow p-5 m-4 rounded-md cursor-pointer items-center"
-        tabIndex={0}
-        role="menuitem"
-        onClick={()=>props.onClick(props.credential)}
-        style={{
-            width: "85%",
-            height: "auto",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            textAlign: "center",
-            padding: "1rem"
-        }}
-    >
+export function VCCardView(props: Readonly<{
+    onClick: (credential: WalletCredential) => void,
+    credential: WalletCredential
+}>) {
+    return (
         <div
-            style={{
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "space-around",
-                width: "100%"
-            }}
+            className="bg-iw-tileBackground grid grid-cols-[1fr_auto_2fr] gap-4 items-center shadow hover:shadow-lg hover:scale-105 hover:shadow-iw-selectedShadow p-4 m-2 rounded-md cursor-pointer"
+            onClick={() => props.onClick(props.credential)}
+            datatest-id="vc-card-vew"
+            tabIndex={0}
+            role="menuitem"
+            onKeyDown={() => props.onClick(props.credential)}
         >
             <img
-                data-testid="ItemBox-Logo"
+                data-testid="issuer-logo"
                 src={props.credential.credentialTypeLogo}
                 alt="Credential Type Logo"
                 className="w-20 h-20"
             />
             <span
                 className="text-sm font-semibold text-iw-title"
-                data-testid="ItemBox-Text"
-            >{props.credential.credentialType}</span>
+                data-testid="credential-type-display-name"
+            >
+                {props.credential.credentialTypeDisplayName}
+            </span>
         </div>
-
-        <span className="text-sm font-semibold text-gray-500 mt-2">{props.credential.issuerName}</span>
-    </div>;
+    );
 }
